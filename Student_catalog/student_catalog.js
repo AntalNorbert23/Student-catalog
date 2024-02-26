9f0415ee6f1f706fe99e21fb6ae112baf0052e5b
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-02-26 21:28:07 +0200

    added subsection funcionality for each student where grades will be added

--- Student_catalog/student_catalog.js
+++ Student_catalog/student_catalog.js
@@ -32,6 +32,7 @@ const lastname=document.getElementById("lastname");
 const idnr=document.getElementById("idnr");
 const studentslist=document.getElementById("students");
 const errortext=document.getElementById("errortext");
+const studentscontainer=document.getElementById("studentscardcontainer");
 let students=[];
 
 
@@ -39,11 +40,13 @@ let students=[];
 function saveData(){
     localStorage.setItem("data", studentslist.innerHTML);
     localStorage.setItem("students",JSON.stringify(students));
+    localStorage.setItem("indstudent",studentscontainer.innerHTML);
 };
 
     //show function
 function showData(){
     studentslist.innerHTML=localStorage.getItem("data");
+    studentscontainer.innerHTML=localStorage.getItem("indstudent");
 }
 
 addstudent.addEventListener("click",(event)=>{
@@ -68,7 +71,54 @@ addstudent.addEventListener("click",(event)=>{
     const anchorvalue=document.createTextNode(lastname.value+" "+firstname.value);
     studentslist.appendChild(anchor);
     anchor.appendChild(anchorvalue); 
+
+    //create card of the student
+    const studentcard=document.createElement("div");
+
+    //create the inputs for the subjects
+    const inputdiv=document.createElement("div");
+
+    const math=document.createElement("input");
+    const eng=document.createElement("input");
+    const biology=document.createElement("input");
+    const physics=document.createElement("input");
+    const geography =document.createElement("input");
+  
+    //add the inputs the type attribute and append to its container
+    const notes=[math,eng,biology,physics,geography];
+    notes.forEach((element)=>{
+        element.setAttribute("type","text");
+        inputdiv.appendChild(element);
+    });
+
+    //create the submit buttons for inputs
+    const buttondiv=document.createElement("div")
+
+    const mathbtn=document.createElement("button");
+    const engbtn=document.createElement("button");
+    const biologybtn=document.createElement("button");
+    const physicsbtn=document.createElement("button");
+    const geographybtn=document.createElement("button");
+
+    //add to the buttons the addnote id and add grade text and append to its container
+    let buttons=[mathbtn,engbtn,biologybtn,physicsbtn,geographybtn];
+    buttons.forEach(element=>{
+        element.classList.add("addnote");
+        element.textContent="Add grade";
+        buttondiv.appendChild(element);
+    });
+
+    //append the content containers to the main container 
+    studentscontainer.appendChild(studentcard);
+    const studentcardcomponents=[inputdiv,buttondiv];
+
+    studentcardcomponents.forEach(element=>{
+        studentcard.appendChild(element);
+    })
     saveData();
+    firstname.value="";
+    lastname.value="";
+    idnr.value="";
     }
 })
 
